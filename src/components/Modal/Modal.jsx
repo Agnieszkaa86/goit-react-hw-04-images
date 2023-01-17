185f8cf2dd7f73aae5d9609b3848730be1d5310e
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-17 09:22:17 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,29 @@
-import {useEffect, useState } from 'react';
+import {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { Overlay, ModalWindow } from './Modal.styled';
 
-export const Modal= (item, )=> {
-  const[CloseModalWindow,setCloseModalWindow]=useState(false)
 
+export function Modal({ closeModalWindow, item, handleKeyDown }) {
   useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape') {
-        setCloseModalWindow();
-      }
-      window.addEventListener('keydown', handleKeyDown);
-      return () => {
-        window.removeEventListener('keydown', handleKeyDown);
+        closeModalWindow();
       }
     };
-  }, []);
-  // useEffect(() => {
-  //   const handleKeyDown = e => console.log("keydown event: ", e);
-  //   document.addEventListener("keydown", handleKeyDown);
-  //   return () => {
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
- 
-    return (
-      <Overlay onClick={handleKeyDown}>
-        <ModalWindow>
-          <img src={item} alt="" />
-        </ModalWindow>
-      </Overlay>
-    );
-  }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  return (
+    <Overlay onClick={handleKeyDown}>
+      <ModalWindow>
+        <img src={item} alt="" />
+      </ModalWindow>
+    </Overlay>
+  );
+}
 
 
 Modal.propTypes = {
