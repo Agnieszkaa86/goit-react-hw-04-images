8045fa1a3821b07969f34780c985305707e1c9b7
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-17 11:35:25 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 
-export function Modal({ closeModalWindow, item, handleKeyDown }) {
+export function Modal({ closeModalWindow, item}) {
   useEffect(() => {
-    const handleKeyDown = e => {
-      if (e.code === 'Escape') {
-        closeModalWindow();
-      }
-    };
-    window.addEventListener("keydown", handleKeyDown);
+   window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
   });
 
+   const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        closeModalWindow();
+      }
+    };
   return (
     <Overlay onClick={handleKeyDown}>
       <ModalWindow>
